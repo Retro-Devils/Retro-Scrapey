b65342d8d29ee7818ef31823e2b5ccc30966ed88
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-09 22:45:59 -0600

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -4,11 +4,15 @@ if [ -f "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh ]; then
 echo "---UPDATING RETRO SCRAPEY NOW---"
 sleep 5
 sudo rm -r "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
-wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu
+sudo rm -r /usr/local/bin/scrapey
+wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu/
 chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
+sudo cp "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh -f /usr/local/bin/scrapey
+cp 
 else
 echo "---INSTALLING RETRO SCRAPEY NOW---"
 sleep 5
-wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu
+wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu/
 chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
+sudo cp "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh -f /usr/local/bin/scrapey
 fi
