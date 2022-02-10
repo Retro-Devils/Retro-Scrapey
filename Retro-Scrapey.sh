116e68aadffb483d700b469bad374d85f32c7709
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-09 23:08:27 -0600

    Update Retro-Scrapey.sh

--- Retro-Scrapey.sh
+++ Retro-Scrapey.sh
@@ -281,9 +281,11 @@ echo "---UPDATING RETRO SCRAPEY NOW---"
 sleep 5
 sudo rm -r "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
 sudo rm -r /usr/local/bin/scrapey
+sudo rm -r "$HOME"/Devils-Box/scripts/Retro-Scrapey.sh
 wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu/
 chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
 sudo cp "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh -f /usr/local/bin/scrapey
+sudo cp "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh -f "$HOME"/Devils-Box/scripts/Retro-Scrapey.sh
 }
 
 main_menu
