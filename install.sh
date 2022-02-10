9431e64d54b63e214ee81a392a157d55a5fbde51
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-09 22:41:22 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 if [ -f "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh ]; then
 echo "---UPDATING RETRO SCRAPEY NOW---"
-sleep 2
+sleep 5
 sudo rm -r "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
 wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu
 chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
 else
 echo "---INSTALLING RETRO SCRAPEY NOW---"
-sleep 2
+sleep 5
 wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu
 chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
 fi
