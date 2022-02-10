7e9428fdffb8ab9f07b2dd98eab5162ae1ed031d
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-09 22:36:24 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P /home/pi/RetroPie/retropiemenu
+if [ -d ""$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh" ] 
+then 
+sudo rm -r "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
+wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu
+chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
+else
+wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu
 chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
